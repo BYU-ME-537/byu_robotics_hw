213f1b76877e5edf16cbbfb2c36773808f7d4ace
Mat Haskell <mhaskell9@gmail.com>
2024-10-25 10:27:11 -0600

    Calculate reach with distances d and a

--- kinematics_hw05.py
+++ kinematics_hw05.py
@@ -1,12 +1,12 @@
-    # copy this code into the SerialArm __init__ function (found right after the "class SerialArm:" statement) 
+    # copy this code into the SerialArm __init__ function (found right after the "class SerialArm:" statement)
     # to define these variables required by the visualization for the arm
         self.reach = 0
         for i in range(self.n):
-            self.reach += np.sqrt(self.dh[i][0]**2 + self.dh[i][2]**2)
+            self.reach += np.sqrt(self.dh[i][1]**2 + self.dh[i][2]**2)
 
         self.max_reach = 0.0
         for dh in self.dh:
-            self.max_reach += norm(np.array([dh[0], dh[2]]))
+            self.max_reach += norm(np.array([dh[1], dh[2]]))
 
 
 
@@ -14,7 +14,7 @@
     def jacob(self, q, index=None, base=False, tip=False):
         """
         J = arm.jacob(q)
-        Description: 
+        Description:
         Returns the geometric jacobian for the frame defined by "index", which corresponds
         to a frame on the arm, with the arm in a given configuration defined by "q"
 
@@ -36,14 +36,14 @@
         # TODO - start by declaring a zero matrix that is the correct size for the Jacobian
         J = np.zeros()
 
-        # TODO - find the current position of the point of interest (usually origin of frame "n") 
-        # using your fk function this will likely require additional intermediate variables than 
-        # what is shown here. 
-        pe = 
+        # TODO - find the current position of the point of interest (usually origin of frame "n")
+        # using your fk function this will likely require additional intermediate variables than
+        # what is shown here.
+        pe =
 
 
         # TODO - calculate all the necessary values using your "fk" function, and fill every column
-        # of the jacobian using this "for" loop. Functions like "np.cross" may also be useful. 
+        # of the jacobian using this "for" loop. Functions like "np.cross" may also be useful.
         for i in range(index):
             # check if joint is revolute
             if self.jt[i] == 'r':
