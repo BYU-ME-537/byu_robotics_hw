837c25d7114336e7ef6532f1de3f891e9eaf3a2e
Mat Haskell <mhaskell9@gmail.com>
2024-10-25 10:25:55 -0600

    Fixed typo in rpy

--- hw04_test_FK_notebook.py
+++ hw04_test_FK_notebook.py
@@ -1,8 +1,8 @@
-# %% [markdown] 
-# # Homework 4 
+# %% [markdown]
+# # Homework 4
 # * Copy the contents of the file "hw04_transforms_SO3.py" into your "transforms.py" file.
-# * Now complete the blank sections that will allow you to convert between Euler angles, Axis/Angle, Quaternions, and Rotation matrices, all of which represent SO3. 
-# * After completion, check that your answers match the ones given below. Review each cell and think about what the operation means. Does it make sense? If not, check with a friend, TA, or professor. 
+# * Now complete the blank sections that will allow you to convert between Euler angles, Axis/Angle, Quaternions, and Rotation matrices, all of which represent SO3.
+# * After completion, check that your answers match the ones given below. Review each cell and think about what the operation means. Does it make sense? If not, check with a friend, TA, or professor.
 
 # %%
 import transforms as tr
@@ -15,7 +15,7 @@ np.set_printoptions(precision=4)
 # # Problem 1:
 
 # Check your implementation of rpy, axis, and quaternion. For the given "R_test", you should get the following for RPY, axis/angle, and quaternion represenations:
-# $$[\psi, \theta, \phi] = \left[\begin{matrix}1.041\\0.147\\0.5299\end{matrix}\right]$$
+# $$[\phi, \theta, \psi] = \left[\begin{matrix}1.041\\0.147\\0.5299\end{matrix}\right]$$
 # $$[\theta, r] = \left[\begin{matrix}1.13\\0.3574\\0.3574\\0.8629\end{matrix}\right] $$
 # $$\mathcal{Q} = \left[\begin{matrix}0.8446\\0.1913\\0.1913\\0.4619\end{matrix}\right] $$
 
@@ -33,7 +33,7 @@ print(tr.R2quat(R_test))
 
 
 # %% [markdown]
-# Now check that rotation about XZY of $\psi=0.787$, $\theta=0.787$, $\phi=0.787$ gives the 
+# Now check that rotation about XZY of $\psi=0.787$, $\theta=0.787$, $\phi=0.787$ gives the
 # following:
 # $$R =  \left[\begin{matrix}0.4984 & -0.7082 & 0.5\\0.8546 & 0.4984 & -0.1459\\-0.1459 & 0.5 & 0.8537\end{matrix}\right]$$
 
@@ -49,13 +49,13 @@ R = tr.euler2R(0.787, 0.787, 0.787, 'xzy')
 print("R for euler2R was:")
 print(R)
 
-# this is just a rotation about the x-axis by pi/2. So checking it should be easy for you. 
+# this is just a rotation about the x-axis by pi/2. So checking it should be easy for you.
 R = tr.axis2R(3.14/2, np.array([1, 0, 0]))
 print("R for axis2R was:")
 print(R)
 
 # quaternions are harder to understand, but after looking at the resulting R matrix,
-# can you tell what axis this is quaternion is rotating about? 
+# can you tell what axis this is quaternion is rotating about?
 R = tr.quat2R(np.array([0.707, 0.707, 0.0, 0.0]))
 print("R for quaternion2R was:")
 print(R)
