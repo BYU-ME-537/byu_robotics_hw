bc8bce2708e387de82e980d3a025f83cd486dde3
Mat Haskell <mhaskell9@gmail.com>
2024-10-25 10:24:16 -0600

    Various visualization updates.
    - Frames are now cylinders
    - Can draw axis labels on frames
    - Can draw an individual axis (for axis angle)
    - Specified variable types for better autocompletion
    - ArmPlayer updates
        - Can handle prismatic joints now
        - Sliders now work with arrow presses on keyboard
        - Sliders now have an associated textbox to enter a value manually
        - Uses joint limits from SerialArm object

--- kinematics.py
+++ kinematics.py
@@ -7,7 +7,7 @@ Kinematics Module - Contains code for:
 John Morrell, Jan 26 2022
 Tarnarmour@gmail.com
 
-modified by: 
+modified by:
 Marc Killpack, Sept 21, 2022 and Sept 21, 2023
 """
 
@@ -18,34 +18,34 @@ pi = np.pi
 
 
 # this is a convenience class that makes it easy to define a function that calculates "A_i(q)", given the
-# DH parameters for link and joint "i" only. 
+# DH parameters for link and joint "i" only.
 class dh2AFunc:
     """
     A = dh2AFunc(dh, joint_type="r")
     Description:
-    Accepts a list of 4 dh parameters corresponding to the transformation for one link 
-    and returns a function "f" that will generate a homogeneous transform "A" given 
+    Accepts a list of 4 dh parameters corresponding to the transformation for one link
+    and returns a function "f" that will generate a homogeneous transform "A" given
     "q" as an input. A represents the transform from link i-1 to link i. This follows
-    the "standard" DH convention. 
+    the "standard" DH convention.
 
     Parameters:
     dh - 1 x 4 list from dh parameter table for one transform from link i-1 to link i,
-    in the order [theta d a alpha] - THIS IS NOT THE CONVENTION IN THE BOOK!!! But it is the order of operations. 
+    in the order [theta d a alpha] - THIS IS NOT THE CONVENTION IN THE BOOK!!! But it is the order of operations.
 
     Returns:
-    f(q) - a function that can be used to generate a 4x4 numpy matrix representing the homogeneous transform 
+    f(q) - a function that can be used to generate a 4x4 numpy matrix representing the homogeneous transform
         from one link to the next
     """
     def __init__(self, dh, jt):
 
         # if joint is revolute implement correct equations here:
         if jt == 'r':
-            # although A(q) is only a function of "q", the dh parameters are available to these next functions 
-            # because they are passed into the "init" function above. 
+            # although A(q) is only a function of "q", the dh parameters are available to these next functions
+            # because they are passed into the "init" function above.
 
-            def A(q): 
+            def A(q):
                 # See eq. (2.52), pg. 64
-                # TODO - complete code that defines the "A" or "T" homogenous matrix for a given set of DH parameters. 
+                # TODO - complete code that defines the "A" or "T" homogenous matrix for a given set of DH parameters.
                 # Do this in terms of the variables "dh" and "q" (so that one of the entries in your dh list or array
                 # will need to be added to q).
 
@@ -56,7 +56,7 @@ class dh2AFunc:
         else:
             def A(q):
                 # See eq. (2.52), pg. 64
-                # TODO - complete code that defines the "A" or "T" homogenous matrix for a given set of DH parameters. 
+                # TODO - complete code that defines the "A" or "T" homogenous matrix for a given set of DH parameters.
                 # Do this in terms of the variables "dh" and "q" (so that one of the entries in your dh list or array
                 # will need to be added to q).
 
@@ -78,18 +78,18 @@ class SerialArm:
 
     def __init__(self, dh_params, jt=None, base=eye, tip=eye, joint_limits=None):
         """
-        arm = SerialArm(dh_params, joint_type, base=I, tip=I, radians=True, joint_limits=None)
-        :param dh: n length list where each entry in list is another list of length 4, representing dh parameters, [theta d a alpha]
+        arm = SerialArm(dh_params, joint_type, base=I, tip=I, joint_limits=None)
+        :param dh: n length list where each entry is another list of 4 dh parameters: [theta d a alpha]
         :param jt: n length list of strings, 'r' for revolute joint and 'p' for prismatic joint
         :param base: 4x4 numpy array representing SE3 transform from world or inertial frame to frame 0
         :param tip: 4x4 numpy array representing SE3 transform from frame n to tool frame or tip of robot
         :param joint_limits: 2 length list of n length lists, holding first negative joint limit then positive, none for
-        not implemented
+        not implemented (values in radians/meters)
         """
         self.dh = dh_params
         self.n = len(dh_params)
 
-        # we will use this list to store the A matrices for each set/row of DH parameters. 
+        # we will use this list to store the A matrices for each set/row of DH parameters.
         self.transforms = []
 
         # assigning a joint type
@@ -104,8 +104,8 @@ class SerialArm:
         # using the code we wrote above to generate the function A(q) for each set of DH parameters
         for i in range(self.n):
             # TODO use the class definition above (dh2AFunc), and the dh parameters and joint type to
-            # make a function and then append that function to the "transforms" list. 
-            f = 
+            # make a function and then append that function to the "transforms" list.
+            f =
             self.transforms.append(f.A)
 
 
@@ -118,26 +118,26 @@ class SerialArm:
     def fk(self, q, index=None, base=False, tip=False):
         """
             T = arm.fk(q, index=None, base=False, tip=False)
-            Description: 
-                Returns the transform from a specified frame to another given a 
+            Description:
+                Returns the transform from a specified frame to another given a
                 set of joint inputs q and the index of joints
 
             Parameters:
                 q - list or iterable of floats which represent the joint positions
-                index - integer or list of two integers. If a list of two integers, the first integer represents the starting JOINT 
+                index - integer or list of two integers. If a list of two integers, the first integer represents the starting JOINT
                     (with 0 as the first joint and n as the last joint) and the second integer represents the ending FRAME
-                    If one integer is given only, then the integer represents the ending Frame and the FK is calculated as starting from 
+                    If one integer is given only, then the integer represents the ending Frame and the FK is calculated as starting from
                     the first joint
                 base - bool, if True then if index starts from 0 the base transform will also be included
                 tip - bool, if true and if the index ends at the nth frame then the tool transform will be included
-            
+
             Returns:
                 T - the 4 x 4 homogeneous transform from frames determined from "index" variable
         """
 
         ###############################################################################################
         # the following lines of code are data type and error checking. You don't need to understand
-        # all of it, but it is helpful to keep. 
+        # all of it, but it is helpful to keep.
 
         if not hasattr(q, '__getitem__'):
             q = [q]
@@ -172,15 +172,15 @@ class SerialArm:
             print("WARNING: starting frame must be less than ending frame!")
             print(f"Starting frame: {start_frame}  Ending frame: {end_frame}")
             return None
-        ###############################################################################################        
+        ###############################################################################################
         ###############################################################################################
 
 
         # TODO - Write code to calculate the total homogeneous transform "T" based on variables stored
-        # in "base", "tip", "start_frame", and "end_frame". Look at the function definition if you are 
-        # unsure about the role of each of these variables. This is mostly easily done with some if/else 
-        # statements and a "for" loop to add the effect of each subsequent A_i(q_i). But you can 
-        # organize the code any way you like.  
+        # in "base", "tip", "start_frame", and "end_frame". Look at the function definition if you are
+        # unsure about the role of each of these variables. This is mostly easily done with some if/else
+        # statements and a "for" loop to add the effect of each subsequent A_i(q_i). But you can
+        # organize the code any way you like.
         T = np.eye(4)
 
         return T
@@ -190,9 +190,9 @@ class SerialArm:
     # the robot that you make.
     def __str__(self):
         """
-            This function just provides a nice interface for printing information about the arm. 
+            This function just provides a nice interface for printing information about the arm.
             If we call "print(arm)" on an SerialArm object "arm", then this function gets called.
-            See example in "main" below. 
+            See example in "main" below.
         """
         dh_string = """DH PARAMS\n"""
         dh_string += """theta\t|\td\t|\ta\t|\talpha\t|\ttype\n"""
@@ -207,9 +207,9 @@ if __name__ == "__main__":
     import time
 
     # Defining a table of DH parameters where each row corresponds to another joint.
-    # The order of the DH parameters is [theta, d, a, alpha] - which is the order of operations. 
-    # The symbolic joint variables "q" do not have to be explicitly defined here. 
-    # This is a two link, planar robot arm with two revolute joints. 
+    # The order of the DH parameters is [theta, d, a, alpha] - which is the order of operations.
+    # The symbolic joint variables "q" do not have to be explicitly defined here.
+    # This is a two link, planar robot arm with two revolute joints.
     dh = [[0, 0, 0.3, 0],
           [0, 0, 0.3, 0]]
 
