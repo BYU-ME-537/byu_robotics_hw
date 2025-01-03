414025696ca50f1e776821e67713a6cca8257d9c
Mat Haskell <mhaskell9@gmail.com>
2025-01-02 20:25:41 -0700

    Update HW 9 to match key

--- hw09_sim_and_control.py
+++ hw09_sim_and_control.py
@@ -1,8 +1,8 @@
-## The template python file for hw09 can be a bit confusing. 
-## Please feel free to only use whatever makes sense to you and 
+## The template python file for hw09 can be a bit confusing.
+## Please feel free to only use whatever makes sense to you and
 ## delete the rest if you don't find it helpful.
 
-# %% [markdown] 
+# %% [markdown]
 # # Homework 9
 
 # %%
@@ -10,7 +10,6 @@ import dynamics as dyn
 from visualization import VizScene
 from scipy.integrate import solve_ivp
 from scipy.io import loadmat
-import time
 from matplotlib import pyplot as pl
 
 import numpy as np
@@ -20,43 +19,43 @@ import numpy as np
 # # Problem #1
 
 # %%
-# set up model 
+# set up model
 
 # defining kinematic parameters for three-link planar robot
 dh = [[0, 0, 0.2, 0],
-     [0, 0, 0.2, 0],
-     [0, 0, 0.2, 0]]
+      [0, 0, 0.2, 0],
+      [0, 0, 0.2, 0]]
 
 joint_type = ['r', 'r', 'r']
 
 link_masses = [1, 0.5, 0.3]
 
 # defining three different centers of mass, one for each link
-r_coms = [np.array([-0.1, 0, 0]), 
-          np.array([-0.1, 0, 0]), 
+r_coms = [np.array([-0.1, 0, 0]),
+          np.array([-0.1, 0, 0]),
           np.array([-0.1, 0, 0])]
 
-# all terms except Izz are zero because they don't matter in the 
-# equations, Ixx, and Iyy are technically non-zero, we just don't 
-# rotate about those axes so it doesn't matter. 
-link_inertias = [np.diag([0, 0, 0.01]), 
-                 np.diag([0, 0, 0.01]), 
-                 np.diag([0, 0, 0.01])]
+# all terms except Izz are zero because they don't matter in the
+# equations, Ixx, and Iyy are technically non-zero, we just don't
+# rotate about those axes so it doesn't matter.
+link_inertias = [np.diag([0, 0, 0.1]),
+                 np.diag([0, 0, 0.1]),
+                 np.diag([0, 0, 0.1])]
 
 # the viscous friction coefficients for B*q_dot are:
 B = np.diag([0.8, 0.3, 0.2])  # but you will need to either include
                               # these in your dynamic arm constructor,
                               # or just use them as a global variable
-                              # in your EOM function for numerical 
+                              # in your EOM function for numerical
                               # integration.
 
 
 # my assumption is that you are using your RNE dynamics
 arm = dyn.SerialArmDyn(dh,
-                    jt=joint_type,
-                    mass=link_masses,
-                    r_com=r_coms,
-                    link_inertia=link_inertias)
+                       jt=joint_type,
+                       mass=link_masses,
+                       r_com=r_coms,
+                       link_inertia=link_inertias)
 
 
 # %% [markdown]
@@ -64,18 +63,20 @@ arm = dyn.SerialArmDyn(dh,
 # %%
 data = loadmat('desired_accel.mat')
 
-t = data['t']
-q = data['q']
+t = data['t'].squeeze() # remove singleton dimensions
+q = data['q'] # probably want to figure out shape of this, relates to axis of np.gradient
 
-qd = np.zeros((len(t), arm.n))
-qdd = np.zeros((len(t), arm.n))
+# TODO you will need to take the derivative of q to get qd and qdd,
+# you can use np.gradient with q and the time vector "t" but you will need
+# to specify the axis (0 or 1) that you are taking the gradient along,
+# e.g. np.gradient(q, t, axis=)
+qd =
+qdd =
 
-# you will need to take the derivative of q to get qd and qdd, 
-# you can use np.gradient with q and the time vector "t"
 # TODO - calc torques for every time step given q, qd, qdd
+torque =
 
-
-# If you have a vector of all torques called "torque", 
+# If you have a vector of all torques called "torque",
 # you can use the following code to plot:
 pl.figure()
 for i in range(arm.n):
@@ -98,18 +99,21 @@ pl.show()
 def robot_grav(t, x):
     x_dot = np.zeros(2*arm.n)
 
-    # TODO - define your EOM function here (that returns x_dot) 
+    # TODO - define your EOM function here (that returns x_dot)
 
     return x_dot
 
-# TODO - perform numerical integration here using "solve_ivp". 
-# When finished, you can use the plotting code below to help you. 
+# TODO - perform numerical integration here using "solve_ivp".
+# When finished, you can use the plotting code below to help you.
 
+
+## NOTE: In all of the plotting code below, I assume x = [qd, q]. If you used
+## x = [q, qd], you will need to change the indexing in the plotting code below.
 
 # making an empty figure
-fig = pl.figure() 
+fig = pl.figure()
 
-# plotting the time vector "t" versus the solution vector for 
+# plotting the time vector "t" versus the solution vector for
 # the three joint positions, entry 3-6 in sol.y
 pl.plot(sol.t, sol.y[arm.n:].T)
 pl.ylabel('joint positions (rad)')
@@ -118,23 +122,17 @@ pl.title('three-link robot falling in gravity field')
 pl.show()
 
 
-# now show the actual robot being simulated in pyqtgraph, this will only 
-# work if you have found the integration solution, and already run 
-# the appropriate launch file:
+# now show the actual robot being simulated in pyqtgraph, this will only
+# work if you have found the integration solution
 # %%
-# visualizing the robot acting under gravity 
+# visualizing the robot acting under gravity
 viz = VizScene()
-time_to_run = tf
-refresh_rate = num_steps/tf
 viz.add_arm(arm)
 
-qs = sol.y[arm.n:].T.flatten()
-
-for i in range(int(refresh_rate * time_to_run)):
-    viz.update(qs=[qs[i]])
-    time.sleep(1.0/refresh_rate)
-    
-viz.hold()
+for i in range(len(sol.t)-1):
+    viz.update(qs=[sol.y[arm.n:,i]])
+    viz.hold(t[i+1]-t[i])
+viz.close_viz()
 
 
 # %% [markdown]
@@ -152,26 +150,30 @@ def eom(t, x, u, qdd_des, qd_des, q_des):
 
 # you can define any q_des, qd_des, and qdd_des you want, but feel free to use this
 # code below if it makes sense to you. I'm just defining q as a function of time
-# and then taking the symbolic derivative. 
+# and then taking the symbolic derivative.
 import sympy as sp
 
 t = sp.symbols('t')
 q_des_sp = sp.Matrix([sp.cos(2*sp.pi*t),
-                   sp.cos(2*sp.pi*2*t),
-                   sp.cos(2*sp.pi*3*t)])
+                      sp.cos(2*sp.pi*2*t),
+                      sp.cos(2*sp.pi*3*t)])
 qd_des_sp = q_des_sp.diff(t)
 qdd_des_sp = qd_des_sp.diff(t)
 
 # turn them into numpy functions so that they are faster and return
-# the right data type. Now we can call these functions at any time "t" 
-# in the "eom" function. 
+# the right data type. Now we can call these functions at any time "t"
+# in the "eom" function.
 q_des = sp.lambdify(t, q_des_sp, modules='numpy')
 qd_des = sp.lambdify(t, qd_des_sp, modules='numpy')
 qdd_des = sp.lambdify(t, qdd_des_sp, modules='numpy')
 
 # %%
-# TODO define three different control functions and numerically integrate for each one. 
+# TODO define three different control functions and numerically integrate for each one.
 # If "sol" is output from simulation, plotting can look something like this:
+
+num_sec =
+time = np.linspace(0, num_sec, num=100*num_sec)
+sol =
 
 pl.figure()
 title = "PD + G control"
@@ -190,18 +192,18 @@ pl.show()
 # %% [markdown]
 # # Problem #3
 
-# If we make a "new" arm object, and call it a different variable name, we can use it 
-# for control, while we simulate with the original model (or vice versa). Here's a way 
+# If we make a "new" arm object, and call it a different variable name, we can use it
+# for control, while we simulate with the original model (or vice versa). Here's a way
 # to add some noise to our parameters:
 percent_err = 0.10
 
-# masses of each link with some error. 
+# masses of each link with some error.
 link_masses = [np.random.uniform(low = link_masses[0]*(1-percent_err), high = link_masses[0]*(1+percent_err)),
                np.random.uniform(low = link_masses[1]*(1-percent_err), high = link_masses[1]*(1+percent_err)),
                np.random.uniform(low = link_masses[2]*(1-percent_err), high = link_masses[2]*(1+percent_err))]
 
 # defining three different centers of mass, one for each link
-r_coms = [np.array([np.random.uniform(low = -0.1*(1+percent_err), high = -0.1*(1-percent_err)), 0, 0]), 
+r_coms = [np.array([np.random.uniform(low = -0.1*(1+percent_err), high = -0.1*(1-percent_err)), 0, 0]),
           np.array([np.random.uniform(low = -0.1*(1+percent_err), high = -0.1*(1-percent_err)), 0, 0]),
           np.array([np.random.uniform(low = -0.1*(1+percent_err), high = -0.1*(1-percent_err)), 0, 0])]
 
